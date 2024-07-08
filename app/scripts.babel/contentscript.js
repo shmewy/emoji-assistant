e9ce5e820a2b6d1058fba74ca170264e21882d41
shmewy <167490786+shmewy@users.noreply.github.com>
2024-07-08 14:42:55 -0400

    Update contentscript.js

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,11 +1,11 @@
-import suppress from './helpers/suppress'
-import basicEmoji from 'node-emoji/lib/emoji.json'
-import getSuggestions from './getSuggestions'
-import getTemplate from './getTemplate'
-import emojilib from 'emojilib/emojis.json'
+import suppress from './helpers/suppress';
+import basicEmoji from 'node-emoji/lib/emoji.json';
+import getSuggestions from './getSuggestions';
+import getTemplate from './getTemplate';
+import emojilib from 'emojilib/emojis.json';
 
-const body = document.body
-let $input, blacklistedDomains = []
+const body = document.body;
+let $input, blacklistedDomains = [];
 
 /**
  * Just a hack to manually trigger emoji-suggest
@@ -15,67 +15,77 @@ let $input, blacklistedDomains = []
  */
 $(window).keydown((e) => {
   if (e.metaKey && e.altKey && e.which === 69) {
-    suppress(e)
-    init()
+    suppress(e);
+    init();
   }
-})
+});
 
-function getEmoji (name) {
-  return basicEmoji[name] || (emojilib[name.replace('-', '_')] && emojilib[name.replace('-', '_')].char)
+function getEmoji(name) {
+  return basicEmoji[name] || (emojilib[name.replace('-', '_')] && emojilib[name.replace('-', '_')].char);
 }
 
-function init () {
-  const hostname = window.location.hostname
+function init() {
+  const hostname = window.location.hostname;
 
   // prevent for the textareas of github.com
   if (hostname === 'github.com') {
-    $('textarea').addClass('es-disabled')
+    $('textarea').addClass('es-disabled');
   }
 
   if (blacklistedDomains.indexOf(window.location.hostname) >= 0) {
-    $(body).removeClass('enable-emoji-assistant').addClass('disable-emoji-assistant')
-    return
+    $(body).removeClass('enable-emoji-assistant').addClass('disable-emoji-assistant');
+    return;
   } else {
-    $(body).removeClass('disable-emoji-assistant').addClass('enable-emoji-assistant')
+    $(body).removeClass('disable-emoji-assistant').addClass('enable-emoji-assistant');
   }
 
-  $input = $('div[contenteditable="true"],input[type=text], textarea').not('.es-disabled')
+  $input = $('div[contenteditable="true"],input[type=text],textarea').not('.es-disabled');
 
-  $input
-    .textcomplete([{
-      id: 'emoji-autosuggest',
-      match: /\B:([\-+\w]*)$/,
-      search: function (term, callback) {
-        getSuggestions(term, callback)
-      },
-      template: function (emoji) {
-        const service = (hostname === 'twitter.com' || hostname === 'tweetdeck.twitter.com')
-          && emoji && emoji.has_img_twitter
-        return getTemplate(emoji, service)
-      },
-      replace: function (value) {
-        return getEmoji(value.short_name) + ' '
-      },
-      index: 1
-    }], {
-      dropdownClassName: 'emoji-assistant',
-      height: 265,
-      maxCount: 14,
-      placementStr: 'top',
-      debounce: 400,
-      zIndex: '999999'
-    })
+  $input.textcomplete([{
+    id: 'emoji-autosuggest',
+    match: /\B:([\-+\w]*)$/,
+    search: function (term, callback) {
+      getSuggestions(term, callback);
+    },
+    template: function (emoji) {
+      const service = (hostname === 'twitter.com' || hostname === 'tweetdeck.twitter.com') && emoji && emoji.has_img_twitter;
+      return getTemplate(emoji, service);
+    },
+    replace: function (value) {
+      return getEmoji(value.short_name) + ' ';
+    },
+    index: 1
+  }], {
+    dropdownClassName: 'emoji-assistant',
+    height: 265,
+    maxCount: 14,
+    placementStr: 'top',
+    debounce: 400,
+    zIndex: '999999'
+  });
 }
-
-let url
 
 // listener for URL change
 chrome.runtime.onMessage.addListener(function (request) {
-  init()
-  url = request.data.url
-})
+  init();
+  url = request.data.url;
+});
+
+// listen for changes to the DOM to reinitialize emoji suggestions
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.addedNodes.length) {
+      init();
+    }
+  });
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
 
 chrome.storage.sync.get('blacklistedDomains', function (response) {
-  blacklistedDomains = response.blacklistedDomains || []
-  init()
-})
+  blacklistedDomains = response.blacklistedDomains || [];
+  init();
+});
